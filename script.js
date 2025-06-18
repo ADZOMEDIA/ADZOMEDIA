8389a81833481e54a230566c38983d9736bfe54c
TEAMADZOMEDIA <adzomediaofficial@gmail.com>
2025-06-18 15:19:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('Adzo Media responsive website loaded');
+
+function openPopup(src) {
+  const popup = document.getElementById('imagePopup');
+  const popupImg = document.getElementById('popupImage');
+  popupImg.src = src;
+  popup.style.display = 'flex';
+}
+
+function closePopup() {
+  document.getElementById('imagePopup').style.display = 'none';
+}
